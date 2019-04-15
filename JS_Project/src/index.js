e618a3a6f94651f9981310c258614ad4beb2b331
nikitabutkevich <44376155+nikitabutkevich@users.noreply.github.com>
2019-04-15 20:44:42 +0300

    step

--- JS_Project/src/index.js
+++ JS_Project/src/index.js
@@ -1,17 +1,25 @@
 $( document ).ready(function() {
-  ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [55.751574, 37.573856],
-            zoom: 9,
-            controls: []
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
-    $('#find').click(function(e){
-myMap.center = '[' + $('#place').innerText + ']';
-});
-});
+	ymaps.ready(function () {
+	    var myMap = new ymaps.Map('map', {
+	            center: [55.751574, 37.573856],
+	            zoom: 9,
+	            controls: []
+	        }, {
+	            searchControlProvider: 'yandex#search'
+	        });
 
 
+	    $('#find').click(function(e){
+			var myGeocoder = ymaps.geocode($('#place').val());
+			myGeocoder.then(function (res) {
+			    myMap.geoObjects.add(res.geoObjects);
 
+			     var location = res.geoObjects.get(0).geometry._coordinates;
+			     myMap.center = location;
+			     myMap.setBounds(location);
+			}, function (err) {
+			    // Обработка ошибки.
+			});
+		});
+	});
 });
