4faa7b8687a2856c1420171b2323c5b974c82d89
user <test@google.com>
2019-04-25 19:38:33 +0300

    step

--- JS_Project/src/step.js
+++ JS_Project/src/step.js
@@ -1,3 +1,4 @@
+if (!searchedLocations === undefined) {
 var searchedLocations = JSON.parse(localStorage.location),
 	list = document.createElement('ul');
 	document.body.prepend(list);
@@ -12,9 +13,22 @@ var searchedLocations = JSON.parse(localStorage.location),
 	$('ul > li > a').click(function(e){
 	localStorage.setItem('history', e.currentTarget.innerHTML);
 });
+};
 
 $('#historyClear').click(function(){
 	for (var i = 0; i < $('ul > li > a').length; i++) {
-		$('ul > li > a')[i].innerHTML = undefined;
+		$('ul > li > a')[i].innerHTML = "";
 	};
+	localStorage.removeItem('location');
 });
+
+if (searchedLocations === undefined) {
+	var nothing = document.body.appendChild(document.createElement('h1')),
+		filler = document.createElement('a');
+
+		nothing.innerHTML = 'Здесь пусто, попробуйте поиск';
+		nothing.appendChild(filler);
+		filler.innerHTML = 'Попробовать =)';
+		filler.href = 'index.html';
+
+}
